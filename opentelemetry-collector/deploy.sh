310204f827c27496f98603dcd4fab4f6e4565505
treezio <upgrades@treez.io>
2025-02-24 02:16:50 +0000

    chore: Update Chart Version for OpenTelemetry Collector to 0.117.0
    Made with ❤️️ by updatecli

--- opentelemetry-collector/deploy.sh
+++ opentelemetry-collector/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 NAMESPACE=tracing
-CHART_VERSION=0.116.0
+CHART_VERSION=0.117.0
 APPLICATION="opentelemetry-collector"
 ENVIRONMENT=`kubectl config current-context | cut -d- -f1`
 DEPLOY=false
